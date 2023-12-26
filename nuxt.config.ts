4eb8267c78dfdcb300c9dd0efee5fb40d8ea07b9
stranavadavid <stranavadavid@protonmail.com>
2023-12-26 19:53:06 +0100

    PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,36 @@ export default defineNuxtConfig({
     runtimeConfig: {
         jwtSecret: '',
     },
+    // supabase: {
+    //     redirectOptions: {
+    //         login: '/login',
+    //         callback: '/confirm',
+    //         exclude: ['/manifest.webmanifest'],
+    //     }
+    // },
+    pwa: {
+        registerType: 'autoUpdate',
+        manifest: {
+            name: 'Dny stesti',
+            short_name: 'Dny stesti',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: 'favicon.icon',
+                    sizes: '250x250',
+                    type: 'image/ico',
+                },
+            ],
+        },
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        },
+        client: {
+            installPrompt: true,
+            // you don't need to include this: only for testing purposes
+            // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+            periodicSyncForUpdates: 20,
+        },
+    },
     devtools: {enabled: false}
 })
